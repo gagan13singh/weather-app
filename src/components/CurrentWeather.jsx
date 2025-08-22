ccc3d0e0dc32e19d3f4c37459bf1d0dd3d1eaa83
gagan13singh <gagandeepsingh130305@gmail.com>
2025-08-22 15:58:27 +0530

    some changes in currentweather aand hourlyweather

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -2,7 +2,7 @@ export default function CurrentWeather({currentWeather}){
     return(
 <div className='weatherSection'>
         <div className='currentWeather'> 
-          <img src= {`icons/${currentWeather.weatherIcon}`}  className='weatherIcon'></img>
+          <img src= {currentWeather.weatherIcon} className='weatherIcon'></img>
           <h2 className='temperature'>{currentWeather.temperature}<span>&deg;C</span></h2>
           <p className='description'>{currentWeather.description}</p>
         </div>
