f6adcc9697a73b098f63937b09311fc23de29220
gagan13singh <gagandeepsingh130305@gmail.com>
2025-08-23 12:32:56 +0530

    debugging

--- src/components/HourlyWeather.jsx
+++ src/components/HourlyWeather.jsx
@@ -1,15 +1,19 @@
-import { weatherCodes } from "../constants"
-const HourlyWeatherItem=({hourlyWeather})=>{
-    const temperature=Math.floor(hourlyWeather.temp_c)
-    const time = hourlyWeather.time.split(" ")[1].substring(0, 5)
-     const weatherIcon=Object.keys(weatherCodes).find(icon=>weatherCodes[icon].includes(hourlyWeather.condition.code))
-    
-    return(
-        <li className='weatherItem'>
-            <p className='time'>{time}</p>
-            <img src={`icons/${weatherIcon}`}  className='weatherIcon'></img>
-            <p className='temperature'>20 &deg;</p>
-          </li>
-    )
-}
+import { weatherCodes } from "../constants";
+
+const HourlyWeatherItem = ({ hourlyWeather }) => {
+  const temperature = Math.floor(hourlyWeather.temp_c);
+  const time = hourlyWeather.time.split(" ")[1].substring(0, 5);
+  const weatherIcon = Object.keys(weatherCodes).find((icon) =>
+    weatherCodes[icon].includes(hourlyWeather.condition.code)
+  );
+
+  return (
+    <li className="weather-item">
+      <p className="time">{time}</p>
+      <img src={`/icons/${weatherIcon}.svg`} className="weather-icon" alt={weatherIcon} />
+      <p className="temperature">{temperature}°C</p>
+    </li>
+  );
+};
+
 export default HourlyWeatherItem;
